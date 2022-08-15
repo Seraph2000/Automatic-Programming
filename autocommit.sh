17c49e8cbc7b3b4ba6667acb8451237993ae20a3
Seraphina Anderson <seraphina.anderson@gmail.com>
2022-08-15 17:37:37 +0100

    Adding a new helper file to the collection.

---
+++ autocommit.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "We start out in the following directory..."
+pwd
+
+repo="Automatic-Programming"
+filename="autocommit.sh"
+
+echo "The real stuff starts here..."
+
+# Test if the repo exists
+if [ ! -d ${repo} ]; then
+
+    git clone git@github.com:Seraph2000/Automatic-Programming.git
+
+else
+    echo "The repository already exists..."
+
+fi
+
+echo "cd'ing into Automatic-Programming..."
+cd Automatic-Programming
+pwd
+
+if [ ! -f ${filename} ]; then
+    cp ../${filename} ${filename}
+    echo "Adding new script to staging area..."
+    git add ${filename}
+    git status
+    echo "Committing the file..."
+    git commit -m "Adding a new helper file to the collection." 
+    echo "Pushing file to the main branch..."
+    git push
+    echo "Done."
+    git status
+else
+    echo "Something went wrong..."
+fi
+
+ls -la
+
+cd ..
+
+
+echo "Do we end up back where we started?"
+pwd
